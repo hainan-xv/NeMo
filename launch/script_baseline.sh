bf20816354ba4758944903071a8bcbf2e7e96c0d
Hainan Xu <hainanx@nvidia.com>
2026-08-23 19:32:34 -0400

    Sync OCI code 20260823_193234

--- launch/script_baseline.sh
+++ launch/script_baseline.sh
@@ -217,7 +217,7 @@ echo "*******STARTING********" \
     model.audio_history_chunks=${AUDIO_HISTORY_CHUNKS} \
     data.dataset.num_delay_frames=${DELAY} \
     ++model.attn_backend=${ATTN_BACKEND} \
-    model.activation_checkpointing=${ACT_CKPT} \
+    ++model.activation_checkpointing=${ACT_CKPT} \
     data.dataset.system_prompt="'${SYSTEM_PROMPT}'" \
     data.train_ds.seed=$LHOTSE_RND_SEED \
     ++trainer.limit_train_batches=$VAL_CHECK_INTERVAL \
